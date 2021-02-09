c5d8fb4ae1b6b81474ed2d6bfdb7c3da1ac3bb4f
キリスト教徒 <77716224+cychrisfds@users.noreply.github.com>
2021-02-09 19:40:17 -0300

    Update help.js

--- src/help.js
+++ src/help.js
@@ -86,6 +86,8 @@ command : *${prefix}simih*
 desc : activate simi mode in the group
 usage : *${prefix}simih 1* to activate simi mode and *${prefix}simih 0* to deactivate simi mode
 note : This command can be used if you are a group admin\n`
+*${prefix}randomhentai*
+
 }
 
 exports.help = help
