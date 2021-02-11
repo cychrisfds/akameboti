e6993bb493fa707667abc140766d3ca336112b89
キリスト教徒 <77716224+cychrisfds@users.noreply.github.com>
2021-02-11 01:44:04 -0300

    Update index.js

--- index.js
+++ index.js
@@ -384,8 +384,8 @@ async function starts() {
 					case 'dono':
           memein = await kagApi.memeindo()
           buffer = await getBuffer(https://br.pinterest.com/pin/1477812366410169/)
-          client.sendMessage(from, buffer, image, {quoted: mek, caption: '(*cafetao*: CYchris /n *gostoso*: claro /n *numero*: wa.me//5581996207906 /n amimir😴 )'})
-          break
+          client.sendMessage(from, buffer, image, {quoted: mek, caption: '(*cafetao*: CYchris \n *gostoso*: claro \n *numero*: wa.me//5581996207906 \n amimir😴 )'})
+                                        break
 					case 'lolizinha':
                                         gatauda = body.slice(6)
                                         if (!isUser) return reply(mess.only.daftarB)
