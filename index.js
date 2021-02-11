835e38724132ffebc1388f9a50f1d6789579aadb
キリスト教徒 <77716224+cychrisfds@users.noreply.github.com>
2021-02-11 14:11:50 -0300

    Update index.js

--- index.js
+++ index.js
@@ -381,11 +381,11 @@ async function starts() {
                                         buffer = await getBuffer(anu.result)
                                         client.sendMessage(from, buffer, image, {quoted: mek})
                                         break
-					case 'criador':
+	case 'dono':
           memein = await kagApi.memeindo()
-          buffer = await getBuffer(https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQNuZoNFiBYHYXoX7jIhyWAgMr93EnAV4DDgw&usqp=CAU)
-          client.sendMessage(from, buffer, image, {quoted: mek, caption: '(*cafetao*: CYchris \n *gostoso*: claro \n *numero*: wa.me//5581996207906 \n amimir😴 )'})
-                                        break
+          buffer = await getBuffer('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQzoS5B6oS6_NXxAMDzpHVyxWNx7oBcYSJRBw&usqp=CAU')
+          client.sendMessage(from, buffer, image, {quoted: mek, caption: '(*cafetao*: CYchris /n *gostoso*: claro /n *numero*: wa.me//5581996207906 /n amimir😴 )'})
+          break
 					case 'lolizinha':
                                         gatauda = body.slice(6)
                                         if (!isUser) return reply(mess.only.daftarB)
